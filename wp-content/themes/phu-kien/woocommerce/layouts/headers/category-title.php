9fe96cdcd333ed8b73570eab4cf0e793934f5f6d
Ngoc Nguyen <ngocnguyenluong89@gmail.com>
2021-07-29 09:27:46 +0700

    Hide breadcum layout

--- wp-content/themes/phu-kien/woocommerce/layouts/headers/category-title.php
+++ wp-content/themes/phu-kien/woocommerce/layouts/headers/category-title.php
@@ -8,13 +8,3 @@ if ( function_exists('yoast_breadcrumb') ) {
 	</div>
 </div>
 
-<div class="shop-page-title category-page-title page-title <?php flatsome_header_title_classes() ?>">
-	<div class="page-title-inner flex-row  medium-flex-wrap container">
-	  <div class="flex-col flex-grow medium-text-center">
-	  	<?php do_action('flatsome_category_title') ;?>
-	  </div>
-	  <div class="flex-col medium-text-center">
-	  	<?php do_action('flatsome_category_title_alt') ;?>
-	  </div>
-	</div>
-</div>
